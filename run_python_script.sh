1be73537216e93f415bbeaa831a247c3a09bf0f8
Johannes Klingebiel <johannes.klingebiel@gmail.com>
2024-08-08 13:48:45 +0200

    additional comments for easier use

--- run_python_script.sh
+++ run_python_script.sh
@@ -1,19 +1,21 @@
 #!/bin/zsh
+# Make sure to edit this line above to your virtual environment
 
+# Path to output directory for debugging (can be deleted if not necessary)
 output_dir=~/PDFExtractor
 
-# Create output directory if it doesn't exist
+# Create output directory if it doesn't exist (can be deleted if not necessary)
 mkdir -p "$output_dir"
 
-# Log the start of the script and current working directory
+# Log the start of the script and current working directory (can be deleted if not necessary)
 echo "Starting script..." > "$output_dir/quick_action_log.txt"
 echo "Current directory: $(pwd)" >> "$output_dir/quick_action_log.txt"
 echo "Script path: $0" >> "$output_dir/quick_action_log.txt"
 echo "Arguments: $@" >> "$output_dir/quick_action_log.txt"
 
 # Define paths
-VENV_PATH="/path/to/your/venv"
-SCRIPT_PATH="/path/to/your/extract_annotations.py"
+VENV_PATH="<YOURPATH>/PDFExtractor/venv"
+SCRIPT_PATH="<YOURPATH>/PDFExtractor/extract_annotations.py"
 
 # Check paths
 echo "VENV Path: $VENV_PATH" >> "$output_dir/quick_action_log.txt"
@@ -25,5 +27,5 @@ source "$VENV_PATH/bin/activate" || { echo "Failed to activate virtual environme
 # Run the Python script with the provided argument
 "$VENV_PATH/bin/python3" "$SCRIPT_PATH" "$1" >> "$output_dir/quick_action_log.txt" 2>&1
 
-# Log the script completion
+# Log the script completion (can be deleted if not necessary)
 echo "Script finished." >> "$output_dir/quick_action_log.txt"
